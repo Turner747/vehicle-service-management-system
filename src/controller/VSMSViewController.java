233e900fc604519e70fe4a15f2b6cae26b039b94
Joshua Turner <joshua.turner2021@gmail.com>
2022-05-21 13:03:39 +1000

    update model classes and constructors, preliminary gui design

--- src/controller/VSMSViewController.java
+++ src/controller/VSMSViewController.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
+package controller;
 
 /**
  *
  * @author josht
  */
-public class VSMSModel
+public class VSMSViewController
 {
     
 }
